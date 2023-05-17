3f5e012e41431f9462572c7cce5ed826608d9fb3
Jared Hinkle <jared.h.hinkle@gmail.com>
2023-05-17 12:45:24 -0700

    updated functions to get more data and distributor template with styling

--- distributor-order-template.php
+++ distributor-order-template.php
@@ -9,7 +9,37 @@ get_header();
 $is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
 
 ?>
+<style>
+    header{
+        display: none;
+    }
+    /* Accordion styles */
+.accordion {
+    background-color: #eee;
+    color: #222;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    text-align: left;
+    border: none;
+    outline: none;
+    transition: 0.4s;
+}
 
+.active, .accordion:hover {
+    background-color: #e02b20;
+    color: #ffffff;
+}
+
+.panel {
+    padding: 0 18px;
+    display: none;
+    background-color: white;
+    overflow: hidden;
+}
+
+
+</style>
 <div id="main-content">
 
     <?php if (!$is_page_builder_used) : ?>
@@ -26,7 +56,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
 
             <?php if (!$is_page_builder_used) : ?>
 
-                <h1 class="entry-title main_title"><?php the_title(); ?></h1>
+                <!-- <h1 class="entry-title main_title"><?php //the_title(); ?></h1> -->
                 <?php
                 $thumb = '';
 
@@ -60,37 +90,72 @@ $is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
 
                         // Display distributor information
                         if ($distributor) {
-                            echo 'Distributor: ' . $distributor->display_name . '<br>';
+                            // echo '<h2>Distributor: </h2><p>' . $distributor->display_name . '</p><br>';
 
                             // Get the distributor's order history
                             $order_history = get_distributor_order_history($distributor_id);
 
                             // Display the order history
+                            $total_price = 0;
                             foreach ($order_history as $order_id => $order_data) {
-                                ?>
-                                <div class="distributor_order_card" style="background-color: #aaa; border-radius: 25px; text-align: left; padding: 25px; color: white; width: 50%;">
-                                <?php
-                                echo 'Order ID: ' . $order_id . '<br>';
-                                echo 'Customer ID: ' . $order_data['customer_id'] . '<br>';
-                                echo 'Name: ' . $order_data['first_name'] . ' ' . $order_data['last_name'] . '<br>';
-                                echo 'Email: ' . $order_data['email'] . '<br>';
-                                echo 'City: ' . $order_data['city'] . '<br>';
-
-                                // Display the order items
-                                foreach ($order_data['items'] as $item) {
-                                    echo 'Order Item: ' . $item['order_item_name'] . '<br>';
-                                    echo 'SKU: ' . $item['sku'] . '<br>';
-                                    echo 'Product Price: ' . $item['product_price'] . '<br>';
-                                    echo '<br>';
+                            // $total_price += $order_data['items']['product_price'];
+                                foreach($order_data['items'] as $item){
+                                    $total_price = $item['total_price'];
                                 }
+                                ?>
+                                <div class="distributor_order_card" style="margin: 10px auto; background-color: #707070; border-radius: 15px; text-align: left; padding: 10px; width: 90%;">
+                                    <button class="accordion"><?php echo '<strong>Order Date:</strong> '. $order_data['date_created'] . '<br><strong>Order ID:</strong> ' . $order_id; ?><br><?php echo '<strong>Total Price:</strong> $'. $total_price;?><br><?php echo '<strong>Order Status:</strong> '. $order_data['status'];?></button>
+                                    <div class="panel">
+                                        <br>
+                                        <!-- Order details -->
+                                        <h2>Order Details:</h2><br>
+                                        <h3>Name:</h3>
+                                        <p><?php echo $order_data['first_name'] . ' ' . $order_data['last_name']; ?></p><hr/>
+                                        <h3>Email:</h3>
+                                        <p><?php echo $order_data['email']; ?></p><hr/>
+                                        <h3>City:</h3>
+                                        <p><?php echo $order_data['city']; ?></p><hr/>
+                                        <h3>Date Created:</h3>
+                                        <p><?php echo $order_data['date_created']; ?></p><hr/>
+                                        <h3>Date Completed:</h3>
+                                        <?php if(isset($order_data['date_completed'])){
 
-                                echo '</div>';
+                                            echo '<p>' . $order_data['date_completed'] . '</p><hr/>';
+
+                                        }else{
+
+                                            echo "<p>Not Complete</p><hr/>"; 
+                                        }
+                                            ?>
+
+                                        <br><br>
+                                        <!-- Order items -->
+                                        <h2>Order Items:</h2><br>
+                                        <?php foreach ($order_data['items'] as $item) : 
+                                            ?>
+                                            <div class="order-item">
+                                                <h3>Order Item:</h3>
+                                                <p><?php echo $item['order_item_name']; ?></p><hr/>
+                                                <h3>SKU:</h3>
+                                                <p><?php echo $item['sku']; ?></p><hr/>
+                                                <h3>Product Price:</h3>
+                                                <p>$<?php echo $item['product_price']; ?></p><hr/>
+                                                <!-- Total pricing -->
+                                                <br>
+                                            </div>
+                                        <?php endforeach; ?>
+                                        <h3>Total Price:</h3>
+                                                <p>$<?php echo $total_price; ?></p>
+                                                <br>
+                                    </div>
+                                </div>
+                                <?php
                             }
                         } else {
-                        echo 'Invalid distributor ID.';
+                        echo '<h2>Invalid distributor ID.</h2>';
                         }
                         } else {
-                        echo 'No distributor ID specified.';
+                        echo '<h2>No distributor ID specified.</h2>';
                         }
                     
                 } else {
@@ -100,8 +165,8 @@ $is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
                 }
                 ?>
 
+                <a href="https://enerlitesstg.wpengine.com/list-of-distributors/">Return to Distributor List</a>
             </div>
-
         </article>
 
     <?php endwhile; ?>
@@ -117,7 +182,23 @@ $is_page_builder_used = et_pb_is_pagebuilder_used(get_the_ID());
 <?php endif; ?>
 
 </div>
+<script>
+    // Accordion functionality
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+</script>
 <?php
 
-get_footer();
+// get_footer();
